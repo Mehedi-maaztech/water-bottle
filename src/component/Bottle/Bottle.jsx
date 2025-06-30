7da6073387d6449db7893fa6e97fb4932afcd6f4
Mehedi Hasan <maaztech71@gmail.com>
2025-06-30 19:53:29 +0600

    updated remove

--- src/component/Bottle/Bottle.jsx
+++ src/component/Bottle/Bottle.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 import './Bottle.css'
 const Bottle = ({bottle , handleAddToCart}) => {
     const {name, price, img} = bottle;
-    //the world is terrific
     return (
-        <div className='bottle'>
+        <div className='bottleC'>
             <h3>Name : {name}</h3>
             <img src={img} alt="" />
             <p>Price : ${price}</p>
